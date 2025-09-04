049178dfb119933fb93b093448b4ec4b9473e43f
Saish Mungase <saishmungase@gmail.com>
2025-09-04 18:49:44 +0530

    add : group implementation done

---
+++ src/mail/group.ts
@@ -0,0 +1,52 @@
+
+export class Group{
+    groups : Map<string, string[]>
+    constructor(){
+        this.groups = new Map<string, string[]>() // () => Makes it compile
+    }
+
+    addGroup(key : string, value : string[]){
+        this.groups.set(key, value)
+    }
+
+    getGroup(key : string) : string[] | undefined {
+        return this.groups.get(key)
+    }
+
+    addBulkGroups(map : Map<string, string[]>){
+        map.forEach((value, key) =>{
+            this.groups.set(key, value)
+        })
+    }
+
+    removeGroup(key : string) : boolean{
+        return this.groups.delete(key);
+    }
+    
+    listGroups(): string[] {
+      return Array.from(this.groups.keys());
+    }
+
+    getAll(): Map<string, string[]> {
+      return this.groups;
+    }
+
+    addGroupMember(key : string, person : string){
+        let group : string[] | undefined = this.getGroup(key);
+        if(!group){
+            console.log("No Group Found with Name " + key);
+            return;
+        }
+        group.push(person)
+        this.groups.set(key, group);
+    }
+
+    removeGroupMember(key : string, person : string){
+        let group = this.getGroup(key);
+        if (group) {
+            group = group.filter(p => p !== person);
+        }
+        return (!group) ? undefined : this.groups.set(key, group);
+    }
+
+}
